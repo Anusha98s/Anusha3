3c98e9dcb852b08d495a1b0b8515c518812fe98f
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 09:51:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Looping_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string AccountNo;
+            AccountNo = Convert.ToString(44123789);
+
+            
+
+            var pie = 3.14;
+
+            var name = "Anu";
+            var Age = 21;
+            var Qualified = true;
+
+        }
+    }
+}
